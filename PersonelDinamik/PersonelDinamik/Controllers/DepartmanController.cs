c53864353db3777b786904f85aad32d2e1769b92
seho2017 <149900043+seho2017@users.noreply.github.com>
2024-08-16 13:30:09 +0300

    Add project files.

---
+++ PersonelDinamik/PersonelDinamik/Controllers/DepartmanController.cs
@@ -0,0 +1,24 @@
+﻿using PersonelDinamik.Models.EntitiyFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelDinamik.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        PersonelDBEntities db=new PersonelDBEntities(); 
+        // GET: Departman
+        public ActionResult Index()
+        {
+            var model=db.Departman.ToList();
+            return View(model);
+        }
+        public ActionResult yeni()
+        {
+            return View();
+        }
+    }
+}
